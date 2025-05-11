cfebf6d94cd212c7afc45b56e914219a52dd87e4
Rohit Sharma <fakeeclipx@gmail.com>
2025-05-11 22:50:26 +0530

    Add files via upload

---
+++ beginner/contour.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+img=cv.imread('photos/c.jpg')
+cv.imshow('City',img)
+
+
+
+
+
+
+
+
+cv.waitKey(0)
--- beginner/transformations.py
+++ beginner/transformations.py
@@ -24,6 +24,54 @@ translated = translate(img, -100, 100)
 # Display the translated image
 cv.imshow('Translated', translated)
 
+
+##rotation
+
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
+
+    # This part must run regardless of whether rotPoint was None
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+# First rotation
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+# Second rotation on the rotated image
+rotated_rotated = rotate(rotated, -90)
+cv.imshow('Rotated Rotated', rotated_rotated)
+
+
+
+
+#resizing
+
+resized=cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized',resized)
+
+
+
+#flipping
+
+flip=cv.flip(img,0)
+cv.imshow('Flip',flip)
+
+
+#croping
+
+cropped=img[200:400,300:400]
+cv.imshow('Cropped',cropped)
+
+
+
+
 # Wait for a key press and close windows
 cv.waitKey(0)
 cv.destroyAllWindows()
