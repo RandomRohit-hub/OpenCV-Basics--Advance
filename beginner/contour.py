87abcedea2a71374fc23e9f2e6164d0c93bc96c1
Rohit Sharma <fakeeclipx@gmail.com>
2025-05-12 10:19:20 +0530

    Add files via upload

--- beginner/contour.py
+++ beginner/contour.py
@@ -2,11 +2,17 @@ import cv2 as cv
 img=cv.imread('photos/c.jpg')
 cv.imshow('City',img)
 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+
+canny=cv.Canny(img,125,125)
+cv.imshow('Canny Edges',canny)
 
 
 
-
-
+contours,heirarchies=cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
 
 
 cv.waitKey(0)
+cv.destroyAllWindows()
