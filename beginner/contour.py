e05e738ac53b4b541e543cf35a2254f8b90554ca
Rohit Sharma <fakeeclipx@gmail.com>
2025-05-12 11:08:07 +0530

    Add files via upload

--- beginner/contour.py
+++ beginner/contour.py
@@ -6,12 +6,34 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 
-canny=cv.Canny(img,125,125)
-cv.imshow('Canny Edges',canny)
+# canny=cv.Canny(img,125,125)
+# cv.imshow('Canny Edges',canny)
+
+# blur=cv.GaussianBlur(gray,(5,5),cv.BORDER_DEFAULT)
+# cv.imshow('BLUR',blur)
+
+# contours,heirarchies=cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+# print(f'{len(contours)} contours(s) found!')
 
 
 
-contours,heirarchies=cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+
+ret,thresh=cv.threshold(gray,125,125,cv.THRESH_BINARY)
+cv.imshow('Thresh',thresh)
+
+
+
+
+
+
+
+
+contours,heirarchies=cv.findContours(thresh,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+print(f'{len(contours)} contours(s) found!')
+
+
+
+
 
 
 cv.waitKey(0)
